bb5850591999239c3eaa08fd7c9f23a304861796
donpepe@totoro <donpepe@totoro>
2018-12-12 06:38:17 +0100

    version 0 in progress

--- app_api/controllers/aerodromes.js
+++ app_api/controllers/aerodromes.js
@@ -8,6 +8,7 @@ const crud = require('./crudator');
 const sendJSON = require('../../app_lib/helpers').sendJSON;
 
 const Model = db.Aerodrome;
+const Galerie = db.Galerie;
 const fieldsArray = ['nom', 'lieu', 'latitude', 'longitude'];
 const hasForeignKeys = false;
 
@@ -24,12 +25,24 @@ exports = Object.assign(exports, basicAPI);
  * referred to the source now refer to the destination
  * @param {number} sourceid 			- id of aerodrome source
  * @param {number} destinationid	- id of aerodrome destination
+ * @return the number of sources that have been deleted (fusioned)
  */
 exports.fusion =  function(req, res) {
-	debug("HERE I AM");
-	sendJSON.ok(res, "NOT YET IMPLEMENTED");
-	//Model
-		//.findAll({include: [{all:true}]})
+	const destinationid = req.params.destinationid;
+	const sourceid = req.params.sourceid;
+	Model
+		.findByPk(destinationid)
+		.then(record => {
+			if (!record) return sendJSON.notFound(res, "destination id: " + destinationid + " not found");
+			else return sendJSON.ok(res, "NOT IMPLEMENTED");
+			 Galerie.update(
+				{aerodromeId: destinationid},
+				{where: {aerodromeId: sourceid}})
+		})
+		.then()
+		.catch();
+	//Galerie
+		//.update({aerodromeId: destinationid
 		//.then(record => sendJSON.ok(res, record))
 		//.catch(err => sendJSON.serverError(res, err));
 }

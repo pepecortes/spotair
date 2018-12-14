910d59ce52e2047abafff00baa9bb69e833dfd0c
donpepe@totoro <donpepe@totoro>
2018-12-14 18:11:02 +0100

    version 0 in progress

--- app_client/index.js
+++ app_client/index.js
@@ -1,3 +1,9 @@
+
+
+//import dotenv from '../dotenv'
+//console.log("env check: " + process.env.DB_DATABASE)
+console.log(process.env.VUE_APP_SOMEKEY)
+
 var welcome = new Vue({ 
     el: '#welcome',
     data: {
@@ -18,9 +24,51 @@ var aerodromes = new Vue({
 		getAerodromes: function() {
 			var vm = this;
 			axios.get('http://localhost:3000/api/aerodromes')
-				.then(function(response){console.log(response.data);vm.aerodromes = response.data})
+				.then(function(response){vm.aerodromes = response.data})
 				.catch(function(err) {console.log(err)})
+		},
+		
+		selectId: function(id) {
+			updateAerodrome.loadMe(id)
 		}
+		
+
+	},
+	
+})
+
+
+var updateAerodrome = new Vue({
+  el: '#update_aerodrome',
+  
+  data: {
+		aerodrome: {}
+	},
+	
+	created: function() {this.getAerodrome()},
+  
+  methods: {
+		getAerodrome: function(id) {
+			if (!id) return;
+			var vm = this;
+			axios.get('http://localhost:3000/api/aerodromes/' + id)
+				.then(function(response){vm.aerodrome = response.data})
+				.catch(function(err) {console.log(err)})
+		},
+		
+		loadMe: function(id) {
+			this.getAerodrome(id)
+		},
+		
+		updateMe: function() {
+			var vm = this;
+			if (!vm.aerodrome.id) return
+			const record = {nom: vm.aerodrome.nom, lieu: vm.aerodrome.lieu}
+			axios.put('http://localhost:3000/api/aerodromes/' + vm.aerodrome.id, record)
+				.then(function(response){vm.aerodrome = response.data})
+				.catch(function(err) {console.log(err)})						
+		},
+		
 	},
 	
 })

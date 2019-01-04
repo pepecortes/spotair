1436361635fe5883690313a704ba52e752d48771
donpepe@totoro <donpepe@totoro>
2019-01-04 08:24:07 +0100

    in progress

--- app_client/src/components/BaseForm.vue
+++ app_client/src/components/BaseForm.vue
@@ -14,27 +14,21 @@ div.baseForm
 			span(slot="no options") Aucun résultat
 		b-button(variant="outline-success", v-on:click="newForm") New
 		
-	b-form(@submit="update", @reset="reset", v-if="toggleForm")
+	b-form(v-if="toggleForm")
 		b-button(variant="outline-warning" v-on:click="clearForm") X
 		
-		slot(name="inputs", :formData='formData', :$v='$v', :checkValidityState='checkValidityState')
+		slot(name="inputs")
 		
-		b-button(type="submit", variant="outline-warning", v-if="!newRecord") Update
-		b-button(type="button", variant="outline-danger", v-on:click="remove", v-if="!newRecord") Delete
-		b-button(type="button", variant="outline-primary", v-on:click="add", v-if="newRecord") Add
-		b-button(type="reset", variant="outline-success") Reset
+		b-button(type="button", variant="outline-warning", v-on:click="$emit('update')", v-if="!newRecord") Update
+		b-button(type="button", variant="outline-danger", v-on:click="$emit('remove')", v-if="!newRecord") Delete
+		b-button(type="button", variant="outline-primary", v-on:click="$emit('add')", v-if="newRecord") Add
+		b-button(type="button", variant="outline-success", v-on:click="$emit('reset')") Reset
 				
 </template>
 
 <script>
-import { confirmDialog, axiosErrorToString } from '../lib/common'
-import { validationMixin } from 'vuelidate'
-import { required, decimal, between } from "vuelidate/lib/validators"
+import { axiosErrorToString } from '../lib/common'
 import VueSelect from 'vue-select'
-
-// Check that both latitude and longitud are present or neither
-const bothCoordinates = (value, vm) => 
-	(vm.latitude && vm.longitude) || (!vm.latitude && !vm.longitude);
 
 export default {
 	
@@ -43,7 +37,7 @@ export default {
 			// path to build the root of API calls (example: "aerodromes")
 			default: "",
 			type: String
-		},
+		}
 	},
 	
 	components: {
@@ -53,8 +47,7 @@ export default {
 	data () {
 		return {
 			selectOptions: [],
-			formData: null, // the data being edited
-			formDataBak: null, // the original selection, in case you need to reset
+			selection: null, // the original selection, in case you need to reset
 			alert: {show: false, text: "", type: "warning"},
 			toggleForm: false,
 			newRecord: false,
@@ -65,32 +58,26 @@ export default {
 	created () {this.getSelectOptions()},
 
 	methods: {
-		
-		// Return the validity state or null if the input is untouched
-		checkValidityState(input) {
-			return (input.$dirty)? !input.$invalid : null
-		},
-		
+				
 		// Init the form with the given selection. Display controls
 		// for update/delete or add depending on the boolean newRecord
 		initForm(selection, newRecord = false) {
 			if (!selection) return
-			this.formData = JSON.parse(JSON.stringify(selection))
-			this.formDataBak = JSON.parse(JSON.stringify(selection))
+			this.$emit('selection', selection)
+			this.selection = JSON.parse(JSON.stringify(selection))
 			this.newRecord = newRecord
 			this.toggleForm = true
-			this.$v.$reset()
 		},
-		
-    // Reset the form values to the initial selection
-		reset(evt) {
-      this.initForm(this.formDataBak, this.newRecord)
-    },
 		
 		// Hide the form, refresh the selector and display it
 		showSelector() {
 			this.getSelectOptions()
 			this.toggleForm = false
+		},
+		
+		// Displays a form alert/info message 
+		showAlert(message="", type="warning") {
+			this.alert = {show: true, text: message, type: type}
 		},
 		
 		clearForm() {
@@ -100,91 +87,35 @@ export default {
 		
 		// Create a fresh form ready for adding new data
 		newForm() {
-			this.formData = {nom: "nom", lieu: "lieu"}
 			var vm = this
-			this.axios.get(process.env.API_URL + 'aerodromes/fresh')
+			this.axios.get(vm.apiPath +'/fresh')
 				.then(response => vm.initForm(response.data, true))
-				.catch(err => vm.alert = {show: true, text: JSON.stringify(err), type: "danger"})
+				.catch(err => vm.alert = {show: true, text: axiosErrorToString(err), type: "danger"})
 		},
 		
 		getSelectOptions() {
 			var vm = this;
-			//this.axios.get(process.env.API_URL + 'aerodromes')
 			this.axios.get(vm.apiPath)
 				.then(response => vm.selectOptions = response.data)
 				.catch(err => vm.alert = {show: true, text: axiosErrorToString(err), type: "danger"})
-		},
+		},    
 		
-		update(evt) {
-			var vm = this;
-      evt.preventDefault()
-      if (!confirmDialog("confirm update?")) return
-      vm.$v.$touch()
-      if (vm.$v.$invalid) {
-				return
-			}
-			const url = process.env.API_URL + 'aerodromes/' + vm.formData.id
-			this.axios.put(url, vm.formData)
-				.then(function(response) {
-					vm.alert = {
-						show: true,
-						text: "Updated: " + response.data.text,
-						type: "success"
-					}
-					vm.showSelector()
-				})
-				.catch(err => vm.alert = {show: true, text: axiosErrorToString(err), type: "danger"})
+		// Reset the form values to the initial selection
+		reset() {
+      this.initForm(this.selection, this.newRecord)
     },
     
-    remove(evt) {
-			var vm = this;
-      evt.preventDefault()
-      if (!confirmDialog("confirm removal?")) return
-      vm.$v.$touch()
-			const url = process.env.API_URL + 'aerodromes/' + vm.formData.id
-			this.axios.delete(url)
+    remove(formData, apiURL) {
+			var vm = this
+			const url = apiURL + formData.id
+			vm.axios.delete(url)
 				.then(function(response) {
-					vm.alert = {
-						show: true,
-						text: "Removed: " + vm.formData.text,
-						type: "success"
-					}
+					vm.showAlert("Removed: " + formData.text, "success")
 					vm.showSelector()
 				})
-				.catch(err => vm.alert = {show: true, text: axiosErrorToString(err), type: "danger"})
-		},
-    
-    add(evt) {
-			var vm = this;
-      evt.preventDefault()
-      vm.$v.$touch()
-      if (vm.$v.$invalid) {
-				return
-			}
-			const url = process.env.API_URL + 'aerodromes/'
-			this.axios.post(url, vm.formData)
-				.then(function(response) {
-					vm.alert = {
-						show: true,
-						text: "Created: " + response.data.text,
-						type: "success"
-					}
-					vm.showSelector()
-				})
-				.catch(err => vm.alert = {show: true, text: axiosErrorToString(err), type: "danger"})
+				.catch(err => vm.showAlert(axiosErrorToString(err), "danger"))
 		},
 		
-	},
-	
-	mixins: [validationMixin],
-	
-	validations: {
-		formData: {
-			nom: {required},
-			lieu: {required},
-			latitude: {decimal, between: between(-90, 90), bothCoordinates},
-			longitude: {decimal, between: between(-180, 180), bothCoordinates},
-    }
 	}
 }
 
--- app_client/src/components/ThemeForm.vue
+++ app_client/src/components/ThemeForm.vue
@@ -1,19 +1,27 @@
 <template lang="pug">
 
 div#galerieForm
-	base-form(api="themes")
-		template(slot="inputs", slot-scope="{formData}")
+	base-form(
+		ref="baseForm",
+		:api="api",
+		v-on:selection="dataSelected",
+		v-on:update="clickMe",
+		v-on:remove="remove", 
+		v-on:add="add", 
+		v-on:reset="reset",
+	)
+		template(slot="inputs")
 			b-form-group(
 				label="Theme",
 				label-for="theme",
 				:invalid-feedback="formData.invalid.theme",
-				:state="checkValidityState($v.theme.theme)"
+				:state="checkValidityState($v.formData.theme)"
 			)
 				b-form-input(
 					id="theme",
 					type="text",
 					v-model.trim="formData.theme",
-					:state="checkValidityState($v.theme.theme)"
+					:state="checkValidityState($v.formData.theme)"
 				)
 
 </template>
@@ -22,6 +30,8 @@ div#galerieForm
 import BaseForm from './BaseForm.vue'
 import { validationMixin } from 'vuelidate'
 import { required } from "vuelidate/lib/validators"
+import { confirmDialog, axiosErrorToString } from '../lib/common'
+import pluralize from 'pluralize'
 
 export default {
 	
@@ -31,22 +41,64 @@ export default {
 	
 	data () {
 		return {
+			model: "theme",
+			formData: {invalid:{}},
 		}
 	},
 	
+	computed: {
+		models () {return pluralize(this.model)},
+		api () {return this.models},
+		apiURL () {return process.env.API_URL + this.api + "/"}
+	},
+	
 	methods: {
+		
+		dataSelected(selection) {
+			this.formData = selection
+			this.$v.$reset()
+		},
+		
+		clickMe() {
+			console.log("CLIKED")
+			console.log("api: " + this.api)
+		},
 	
 		// Return the validity state or null if the input is untouched
 		checkValidityState(input) {
-			console.log("CHECKING VALIDITY")
 			return (input.$dirty)? !input.$invalid : null
 		},
+		
+		// Reset the form to the initial selection
+		reset() {
+			this.$refs.baseForm.reset()
+			this.$v.$reset()
+		},
+    
+    add() {
+			var vm = this
+      vm.$v.$touch()
+      if (vm.$v.$invalid) return
+			this.axios.post(vm.apiURL, vm.formData)
+				.then(function(response) {
+					vm.$refs.baseForm.showAlert("Created: " + response.data.text, "success")
+					vm.$refs.baseForm.showSelector()
+				})
+				.catch(err => vm.alert = {show: true, text: axiosErrorToString(err), type: "danger"})
+		},
+    
+    remove() {
+      if (!confirmDialog("confirm removal?")) return
+      this.$v.$touch()
+      this.$refs.baseForm.remove(this.formData, this.apiURL)
+		},
+		
 	},
 	
 	mixins: [validationMixin],
 	
 	validations: {
-		theme: {
+		formData: {
 			theme: {required},
     }
 	}

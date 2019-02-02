9d24167d630ae1620cae2f3902aca81b2cdc8944
donpepe <donpepe@mazinger>
2019-02-02 09:11:25 +0100

    corrected fusion of photographes

--- app_api/controllers/photographes.js
+++ app_api/controllers/photographes.js
@@ -6,12 +6,23 @@ const debug = require('debug')('app:api:controllers:photographes')
 const db = require('../models/db')
 const sendJSON = require('../../app_lib/helpers').sendJSON
 const ModelController = require('./modelController')
+const dbReplaceReference = require('../../app_lib/helpers').dbReplaceReference
 
 var controller = new ModelController(db.Photographe)
 
 // building other API calls...
 
-// fusion
-controller.fusion = ModelController.buildFusionController('photographeId', db.Photographe, db.Photo)
+// fusion. Do not use the standard fusion, as a photographe is referred
+// both in Photos and Users.
+// replace the reference in photos and remove the user
+controller.fusion = async function(req, res) {
+	const sourceid = req.params.sourceid
+	const destinationid = req.params.destinationid
+	var promise1 = dbReplaceReference(db.Photo, db.Photographe, 'photographeId', sourceid, destinationid)
+	var promise2 = db.User.destroy({where: {photographeId: sourceid}})
+	Promise.all([promise1, promise2])
+		.then(result => sendJSON.ok(res, result[0]))
+		.catch(err => sendJSON.serverError(res, err))
+}
 
 module.exports = controller

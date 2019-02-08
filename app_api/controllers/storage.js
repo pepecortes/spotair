d1e90c71c0108315617310f8f885f37cd4d0b018
donpepe@totoro <donpepe@totoro>
2019-02-08 18:13:35 +0100

    openstack in progress

--- app_api/controllers/storage.js
+++ app_api/controllers/storage.js
@@ -2,6 +2,9 @@ const debug = require('debug')('app:api:storage')
 const helpers = require('../../app_lib/helpers')
 const sendJSON = helpers.sendJSON
 const fs = require('fs')
+const formidable = require('formidable')
+
+const LOCAL_STORAGE = (process.env.STORAGE === "LOCAL") 
 
 const SwiftClient = require('openstack-swift-client')
 const authURL = "http://swift:8080/auth/v1.0"
@@ -21,52 +24,61 @@ var configOVH = {
 /**
  * @function
  * @desc Store the file in the current container
- * @param {string} fileName
- * @param {string} filePath		- (temp) path of the file to store
+ * @param {Object} file		- File object given by the http req
+ * @param {String} path		- Path where the file is stored in the container
  * @return {Promise} Promise that resolves or error
  */
-function storeToContainer(fileName, filePath) {
-	const client = new SwiftClient(authenticator)
-	const container = client.container(process.env.CONTAINER_NAME)
-	var stream = fs.createReadStream(filePath)
-	return container.create(fileName, stream)
+function storeToContainer(file) {
+	var stream = fs.createReadStream(file.path)
+	const container = getContainer()
+	if (LOCAL_STORAGE) return container.create(file.name, stream)
+	// if OVH remote storage...
+	function putFilePromise() {
+		const path = "/" + process.env.CONTAINER_NAME + "/" + file.name
+		return new Promise((resolve, reject) =>
+			container.putStream(stream, path, (err, data) => {
+				if (err !== null) reject(err)
+				else resolve(data)
+			}))
+	}
+	return getOVHToken(container).then(() => putFilePromise())
 }
 
 function listContainer() {
-	//const client = new SwiftClient(authenticator)
-	//const container = client.container(process.env.CONTAINER_NAME)
-	//return container.list()
-	
-	//ERROR: this actually list containers, not files
-	var storage = new OVHStorage(configOVH)
-	
-	var getTokenPromise = new Promise((resolve, reject) => storage.getToken((err, data) => {
-		if (err !== null) reject(err)
-		else resolve(data)
-	}))
-	
-	var getListPromise = new Promise((resolve, reject) => storage.getFileList((err, data) => {
-		if (err !== null) reject(err)
-		else resolve(data)
-	}))
-	
-	return getTokenPromise.getListPromise
-	
-	//var promise = new Promise(function(resolve, reject) {
-		//storage.getToken(function(err) {
-			//if (err !== null) reject(err)
-			//else {
-				//storage.getFileList("/", function(err, data) {
-					//if (err !== null) reject(err)
-					//else resolve(data)
-				//})
-			//}
-		//})
-	//})
-	//return promise
-	
-	
+	const container = getContainer()
+	if (LOCAL_STORAGE) return container.list()
+	// if OVH remote storage...
+	function getListPromise() {
+		return new Promise((resolve, reject) =>
+			container.getFileList("/" + process.env.CONTAINER_NAME, (err, data) => {
+				if (err !== null) reject(err)
+				else resolve(data)
+			}))
+	}
+	return getOVHToken(container).then(() => getListPromise())
 }
+
+/**
+ * @function
+ * @desc Returns a container, whether local or remote depends in the environment variables
+ */
+function getContainer() {
+	if (LOCAL_STORAGE) return new SwiftClient(authenticator).container(process.env.CONTAINER_NAME)
+	else return new OVHStorage(configOVH)
+}
+
+/** @function
+ * @desc Needed for getting access to OVH container
+ * @param {Container} container
+ * @return {Promise} Promise that resolves if the token is obtained
+ */
+function getOVHToken(container) {
+	return new Promise((resolve, reject) => container.getToken((err, data) => {
+		if (err !== null) reject(err)
+		else resolve(data)
+	}))
+}
+
 
 /**
  * @Object
@@ -77,16 +89,11 @@ function listContainer() {
 var storageController = {}
 
 storageController.postFile = function(req, res) {
-	var formidable = require('formidable')
 	var form = new formidable.IncomingForm()
 	form.parse(req, function(err, fields, files) {
-		var file = files.myfile
-		storeToContainer(file.name, file.path)
+		storeToContainer(files.myfile)
 			.then(output => sendJSON.ok(res, output))	
-			.catch(err => {
-				debug(err)
-				sendJSON.serverError(res, err)
-			})
+			.catch(err => sendJSON.serverError(res, err))
 	})
 }
 

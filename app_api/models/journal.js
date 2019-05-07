a45fa6b38a8f901a7c204b8e82741cde97929554
donpepe@totoro <donpepe@totoro>
2019-05-07 18:50:55 +0200

    journal model. url can be null

--- app_api/models/journal.js
+++ app_api/models/journal.js
@@ -15,7 +15,7 @@ module.exports = function(sequelize, DataTypes) {
     },
     url: {
 			type: DataTypes.STRING,
-			allowNull: false,
+			allowNull: true,
 			defaultValue: "",
 		},			
     createdAt: {

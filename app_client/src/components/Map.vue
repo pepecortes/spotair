2fe2f2dfb25d2f4d56b9cc130e4059322fe1d15f
donpepe@totoro <donpepe@totoro>
2019-06-27 07:09:55 +0200

    Testing Google Maps

--- app_client/src/components/Map.vue
+++ app_client/src/components/Map.vue
@@ -14,12 +14,16 @@
 		
 		br
 		
+		div(id="gmapFilter")
+			button(@click='centerClicked') Recentrer
+			button() Reset
+		
 		gmap-map(
+			style="width: 500px; height: 300px",
 			ref='mapRef',
-			:center="center",
-			:zoom="7",
-			map-type-id="terrain",
-			style="width: 500px; height: 300px"
+			:center='center',
+			:zoom='zoom',
+			:options='options',
 		)
 			gmap-marker(:key="index", v-for="(m, index) in markers", :position="m.position", @click="center=m.position")
 		
@@ -39,17 +43,23 @@ export default {
 	
 	data() {
 		return {
-      center: {lat:0, lng:0},
+      center: {lat:15, lng:15},
+      zoom: 3,
       markers: [],
       places: [],
+      MAP: null,
       currentPlace: null,
-      options:  {zoom: 12, style: "width:100%; height: 400px;"},
     }
 	},
 	
 	computed: {
 		google: gmapApi,
-    //center() {return (this.google)? (new this.google.maps.LatLng(45.508, -73.587)) : {lat:0, lng:0}},
+		
+		options() {
+			if (!this.google) return {}
+			return {mapTypeControlOptions: {position: this.google.maps.ControlPosition.TOP_RIGHT}}
+		},
+		
 	},
 		
 	beforeMount() {
@@ -59,15 +69,68 @@ export default {
 		this.mapAvailable = true
 		var vm = this
 		this.$refs.mapRef.$mapPromise.then(map => {
-			vm.center = new vm.google.maps.LatLng(45.508, -73.587)
-			const m1 = new vm.google.maps.Marker({position: vm.center})
-			vm.markers.push(m1)
+			this.MAP = map
+			this.initializeMap()
+			//vm.center = new vm.google.maps.LatLng(45.508, -73.587)
+			//const m1 = new vm.google.maps.Marker({position: vm.center})
+			//vm.markers.push(m1)
 		}
 			
 		)
 	},
 
 	methods: {
+		
+			
+		customControl(controlDiv, text, customFunction) {
+
+			// Set CSS for the control border.
+			var controlUI = document.createElement('div');
+			controlUI.className = "gMapControlBorder";
+			controlUI.title = text;
+			controlDiv.appendChild(controlUI);
+
+			// Set CSS for the control interior.
+			var controlText = document.createElement('div');
+			controlText.className = "gMapControlInterior";
+			controlText.innerHTML = text;
+			controlUI.appendChild(controlText);
+
+			// Setup the click event listeners
+			//controlUI.addEventListener('click', customFunction);
+			
+			return controlDiv
+		},
+		
+		initializeMap() {
+			var testDiv = document.createElement('div')
+			var newContent = document.getElementById('gmapFilter');
+			
+			var searchControlDiv = document.createElement('div')
+			var searchControl = this.customControl(searchControlDiv, "Recherche", null)
+			newContent = searchControl
+			
+			
+			//this.GMAP.addCustomControl(dom);
+			testDiv.appendChild(newContent)
+			this.MAP.controls[this.google.maps.ControlPosition.TOP_LEFT].push(testDiv);
+			this.zoomAndCenter()			
+		},
+		
+		centerClicked() {
+			this.zoomAndCenter()
+		},
+		
+		zoomAndCenter(zoom=3, lat=15, long=15) {
+			/**
+			 * Zoom and Center a given Google Map object
+			 * inputs are self explanatory and get defaults
+			 */
+			 var center = new this.google.maps.LatLng(lat, long)
+			 this.MAP.panTo(center)
+			 this.MAP.setZoom(zoom)
+		},
+		
 		
 		// receives a place object via the autocomplete component
     setPlace(place) {

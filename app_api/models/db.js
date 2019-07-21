a7fe203aa7237de7c309512a49af9ed45c6f9b1c
donpepe@arcturus <donpepe@arcturus>
2019-07-21 09:32:38 +0200

    Update of several packages to last versions to correct node vulnerabilities

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -13,7 +13,6 @@ const sequelize = new Sequelize({
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  operatorsAliases: false,
   define: {
 		charset: 'utf8',
     collate: 'utf8_general_ci',

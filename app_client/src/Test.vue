571519ee86f3381bcfe84c191cb5238423ea821e
donpepe@arcturus <donpepe@arcturus>
2019-08-25 16:45:17 +0200

    Add PhotoDataEdit component

--- app_client/src/Test.vue
+++ app_client/src/Test.vue
@@ -2,83 +2,26 @@
 	div
 		b-button(v-on:click='test') TEST
 		b-button(v-on:click='test2') TEST2
-		div(class='testelement')
-			editor-input(
-				ref='photographeInput',
-				title="Photographe",
-				apiCall="photographes",
-				v-model="mutablePhoto.photographe",
-				:adminForm='admin.photographe',
-				:state='!$v.mutablePhoto.photographe.$invalid',
-			)
-			editor-input(
-				ref='compagnieInput',
-				title="Compagnie",
-				apiCall="compagnies",
-				v-model="mutablePhoto.compagnie",
-				:adminForm='admin.compagnie',
-				:state='!$v.mutablePhoto.compagnie.$invalid',
-			)
-			editor-input(
-				ref='appareilInput',
-				title="Appareil",
-				apiCall="appareils",
-				v-model='mutablePhoto.appareil',
-				:adminForm='admin.appareil',
-				:state='!$v.mutablePhoto.appareil.$invalid',
-			)
-			editor-input(
-				ref='galerieInput',
-				title="Galerie",
-				apiCall="galeries",
-				v-model="mutablePhoto.galerie",
-				:adminForm='admin.galerie',
-				:state='!$v.mutablePhoto.galerie.$invalid',
-			)
+		photo-data-edit(
+			ref='TEST',
+			v-model='photo',
+		)
 </template>
 
 <script>
-import { validationMixin } from 'vuelidate'
-import { required } from 'vuelidate/lib/validators'
-
-import EditorInput from './components/EditorInput.vue'
-import AppareilForm from './components/AppareilForm.vue'
-import GalerieForm from './components/GalerieForm.vue'
-import CompagnieForm from './components/CompagnieForm.vue'
-import PhotographeForm from './components/PhotographeForm.vue'
+import PhotoDataEdit from './components/PhotoDataEdit.vue'
 
 export default {
-	
-	components: {
-		'editor-input': EditorInput,
-	},
-		
-	beforeMount() {},
-	
-	mounted () {},
 	
 	data() {
 		return {
 			photo: {},
-			mutablePhoto: {},
-			admin: {appareil: AppareilForm, galerie: GalerieForm, compagnie: CompagnieForm, photographe: PhotographeForm},
 		}
 	},
 	
-	watch: {
-    photo() {
-			this.mutablePhoto = this.photo
-			this.$refs.photographeInput.setInitialValue(this.photo.photographe, true)
-			this.$refs.compagnieInput.setInitialValue(this.photo.compagnie, true)
-			this.$refs.appareilInput.setInitialValue(this.photo.appareil, true)
-			this.$refs.galerieInput.setInitialValue(this.photo.galerie, true)
-			},
+	components: {
+		'photo-data-edit': PhotoDataEdit,
 	},
-	
-	computed: {
-	},
-	
-	mixins: [validationMixin],
 	
 	methods: {
 		
@@ -89,20 +32,9 @@ export default {
 		},
 		
 		test2() {
-			console.log("NEW CONTENT: " + JSON.stringify(this.mutablePhoto.photographe))
+			console.log("PHOTO: " + JSON.stringify(this.photo.photographe))
 		},
 		
-	}, 
-	
-	validations() {
-		return {mutablePhoto: 
-			{
-				photographe: {required},
-				compagnie: {required},
-				appareil: {required},
-				galerie: {required},
-			}
-		}
 	},
 	
 }
@@ -110,10 +42,4 @@ export default {
 </script>
 
 <style lang="scss">
-
-.testelement {
-	width: 500px;
-	background-color: blanchedalmond;
-}
-
 </style>
---
+++ app_client/src/components/PhotoDataEdit.vue
@@ -0,0 +1,124 @@
+<template lang="pug">
+	div
+		div(class='testelement')
+			editor-input(
+				ref='photographeInput',
+				title="Photographe",
+				apiCall="photographes",
+				v-model="mutablePhoto.photographe",
+				:adminForm='admin.photographe',
+				:state='!$v.mutablePhoto.photographe.$invalid',
+			)
+			editor-input(
+				ref='compagnieInput',
+				title="Compagnie",
+				apiCall="compagnies",
+				v-model="mutablePhoto.compagnie",
+				:adminForm='admin.compagnie',
+				:state='!$v.mutablePhoto.compagnie.$invalid',
+			)
+			editor-input(
+				ref='appareilInput',
+				title="Appareil",
+				apiCall="appareils",
+				v-model='mutablePhoto.appareil',
+				:adminForm='admin.appareil',
+				:state='!$v.mutablePhoto.appareil.$invalid',
+			)
+			editor-input(
+				ref='galerieInput',
+				title="Galerie",
+				apiCall="galeries",
+				v-model="mutablePhoto.galerie",
+				:adminForm='admin.galerie',
+				:state='!$v.mutablePhoto.galerie.$invalid',
+			)
+</template>
+
+<script>
+import { validationMixin } from 'vuelidate'
+import { required } from 'vuelidate/lib/validators'
+
+import EditorInput from './EditorInput.vue'
+import AppareilForm from './AppareilForm.vue'
+import GalerieForm from './GalerieForm.vue'
+import CompagnieForm from './CompagnieForm.vue'
+import PhotographeForm from './PhotographeForm.vue'
+
+export default {
+	
+	props: {
+		value: {
+			default: null
+		},
+	},
+	
+	components: {
+		'editor-input': EditorInput,
+	},
+		
+	beforeMount() {},
+	
+	mounted () {},
+	
+	data() {
+		return {
+			photo: {},
+			mutablePhoto: {},
+			admin: {
+				appareil: AppareilForm,
+				galerie: GalerieForm,
+				compagnie: CompagnieForm,
+				photographe: PhotographeForm
+			},
+		}
+	},
+	
+	watch: {
+    value() {
+			this.mutablePhoto = this.value
+			this.$refs.photographeInput.setInitialValue(this.value.photographe, true)
+			this.$refs.compagnieInput.setInitialValue(this.value.compagnie, true)
+			this.$refs.appareilInput.setInitialValue(this.value.appareil, true)
+			this.$refs.galerieInput.setInitialValue(this.value.galerie, true)
+			},
+			
+		mutablePhoto(newValue, oldValue) {
+			console.log("mutablePhoto changed: " + newValue.photographe.text)
+		},
+		
+	},	
+	
+	computed: {
+		KOKO() {
+			return (this.$refs.photographeInput.selectionIsLegal)
+		},
+	},
+	
+	mixins: [validationMixin],
+	
+	methods: {}, 
+	
+	validations() {
+		return {mutablePhoto: 
+			{
+				photographe: {required},
+				compagnie: {required},
+				appareil: {required},
+				galerie: {required},
+			}
+		}
+	},
+	
+}
+
+</script>
+
+<style lang="scss">
+
+.testelement {
+	width: 500px;
+	background-color: blanchedalmond;
+}
+
+</style>

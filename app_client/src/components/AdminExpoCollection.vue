8e388841b1ee2afa2833b64d5eb73fc6324469b7
donpepe <donpepe@spica>
2019-09-05 08:58:17 +0200

    AdminExpoCollection in progress

--- app_client/src/components/AdminExpoCollection.vue
+++ app_client/src/components/AdminExpoCollection.vue
@@ -2,16 +2,22 @@
 	extends BaseExpoCollection.pug
 	
 	block admin
-		h1 THIS IS ADMIN
+		h1 THIS IS ADMIN: {{ photoSelected.id }}
+		photo-data-edit(v-model='photoSelected')
 		
 </template>
 
 <script>
 import BaseExpoCollection from './BaseExpoCollection.vue'
+import PhotoDataEdit from './PhotoDataEdit.vue'
 
 export default {
 	
 	extends: BaseExpoCollection,
+	
+	components: {
+		'photo-data-edit': PhotoDataEdit,
+	},	
 	
 }
 </script>

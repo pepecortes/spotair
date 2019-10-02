1457da2de4cfb2ff93d12d22617beb0ace190257
donpepe <donpepe@spica>
2019-10-02 09:47:41 +0200

    Add constants.js

---
+++ app_lib/constants.js
@@ -0,0 +1,30 @@
+/**
+ * Common constants
+ * @module /app_lib/constants
+ */
+
+var exports = {};
+
+/**
+ * @constant
+ * @desc describe several image types
+ */
+const imgType = {
+	picture: 0,
+	upload: 1,
+	thumbnail: 2,	
+}
+exports.imgType = imgType
+
+/**
+ * @constant
+ * @desc describe several search types
+ */
+const SearchType = {
+	FTS: 0,
+	ID: 1,
+	RECENT: 2,	
+}
+exports.SearchType = SearchType
+
+module.exports = exports;

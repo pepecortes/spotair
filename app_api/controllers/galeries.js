01c49738acee56450db15bec7aa622cf2ea3fc28
donpepe <donpepe@spica>
2019-10-23 11:09:03 +0200

    Add additional API calls for galeries

--- app_api/controllers/galeries.js
+++ app_api/controllers/galeries.js
@@ -46,6 +46,52 @@ controller.byAnnee = function(req, res) {
 // fusion
 controller.fusion = ModelController.buildFusionController('galerieId', db.Galerie, db.Photo)
 
+/**
+ * @function recent
+ * @desc Returns the last 'limit' (default=15) recently created galeries
+ */
+controller.recent = async function(req, res) {
+	const limit = (req.params.limit)? parseInt(req.params.limit) : 15
+	db.Galerie
+		.findAll({
+				limit: limit,
+				order:[['createdAt', 'DESC']],
+				include: [{all:true, nested:true}]
+		})
+		.then(record => sendJSON.ok(res, record))
+		.catch(err => sendJSON.serverError(res, err))
+}
+
+/**
+ * @function musees
+ * @desc Returns the galeries corresponding to musees
+ */
+controller.musees = async function(req, res) {
+	db.Galerie
+		.findAll({
+				where: {anneeId: 22},
+				order:[['createdAt', 'DESC']],
+				include: [{all:true, nested:true}]
+		})
+		.then(record => sendJSON.ok(res, record))
+		.catch(err => sendJSON.serverError(res, err))
+}
+
+/**
+ * @function collectors
+ * @desc Returns the galeries corresponding to collectors
+ */
+controller.collectors = async function(req, res) {
+	db.Galerie
+		.findAll({
+				where: {anneeId: 23},
+				order:[['createdAt', 'DESC']],
+				include: [{all:true, nested:true}]
+		})
+		.then(record => sendJSON.ok(res, record))
+		.catch(err => sendJSON.serverError(res, err))
+}
+
 module.exports = controller
 
 
--- app_api/routes.js
+++ app_api/routes.js
@@ -85,12 +85,17 @@ module.exports = function(passport) {
 	
 	// Create additional routes
 	router.get('/appareils/byAvion/:id(\\d+)', (req, res) => ctrlAppareils.byAvion(req, res))
-	router.get('/galeries/spotair', (req, res) => ctrlGaleries.allSpotair(req, res))
-	router.get('/galeries/byAerodrome/:id(\\d+)', (req, res) => ctrlGaleries.byAerodrome(req, res))
-	router.get('/galeries/byAnnee/:id(\\d+)', (req, res) => ctrlGaleries.byAnnee(req, res))
 	router.get('/photographes/actifs', (req, res) => ctrlPhotographes.onlyActives(req, res))
 	router.put('/users/setPassword/:id(\\d+)', (req, res) => ctrlUsers.setPassword(req, res))
 	router.get('/users/byLogin/:username', (req, res) => ctrlUsers.byLogin(req, res))
+	
+	//Routes for querying galeries
+	router.get('/galeries/spotair', (req, res) => ctrlGaleries.allSpotair(req, res))
+	router.get('/galeries/byAerodrome/:id(\\d+)', (req, res) => ctrlGaleries.byAerodrome(req, res))
+	router.get('/galeries/byAnnee/:id(\\d+)', (req, res) => ctrlGaleries.byAnnee(req, res))
+	router.get('/galeries/musees', (req, res) => ctrlGaleries.musees(req, res))
+	router.get('/galeries/collectors', (req, res) => ctrlGaleries.collectors(req, res))
+	router.get('/galeries/recent/:limit(\\d{0,})', (req, res) => ctrlGaleries.recent(req, res))
 	
 	// Routes for managing visible & uploaded photos
 	router.get('/photos/recent/:limit(\\d{0,})', (req, res) => ctrlPhotos.recent(req, res))

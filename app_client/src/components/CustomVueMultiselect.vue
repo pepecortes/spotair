6bc38d513de91ef2961558a96239abe70a8351ef
donpepe@totoro <donpepe@totoro>
2020-01-29 09:47:48 +0100

    VueMultiselect component customized for styling and default texts

---
+++ app_client/src/components/CustomVueMultiselect.vue
@@ -0,0 +1,70 @@
+<script>
+import VueSelect from 'vue-multiselect'
+import 'vue-multiselect/dist/vue-multiselect.min.css'
+
+export default {
+	extends: VueSelect,
+	
+	props: {
+		
+    /**
+     * Equivalent to the `placeholder` attribute on a `<select>` input.
+     * @default 'Select option'
+     * @type {String}
+     */
+    placeholder: {
+      type: String,
+      default: "Sélectionnez une option"
+    },
+    
+    selectLabel: {
+      type: String,
+      default: "appuyez sur entrée pour sélectionner"
+    },
+    
+    selectedLabel: {
+      type: String,
+      default: "sélectionné"
+    },
+    
+    deselectLabel: {
+      type: String,
+      default: "appuyez sur entrée pour désélectionner"
+    },
+    
+	}
+}
+</script>
+
+<style lang="scss">
+@import '../styles/colors.scss';
+
+.multiselect,
+.multiselect__input,
+.multiselect__single {
+  font-size: 0.95rem;
+}
+
+.multiselect__tags {
+  font-size: 1.0rem;
+  padding-top: 0.4rem;
+  padding-bottom: 0.4rem;
+  min-height: unset;
+  border-color: $gray-400;
+}
+
+.multiselect__placeholder {
+	margin-bottom: 0px;
+	padding-top: 0px;
+	color: $gray-600;
+}
+
+.multiselect__tag-icon:after {
+  font-size: 1.0rem;
+}
+
+.multiselect__option:after {
+  font-size: 1.0rem;
+}
+
+</style>
